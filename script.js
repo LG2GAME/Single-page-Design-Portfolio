d2caa5e3ca6e5f3db5d12f6b3ab64a2623483eb2
ark.gor <102352533+LG2GAME@users.noreply.github.com>
2022-11-05 20:59:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,33 @@ burger.addEventListener("click", function(){
     mobileNav.classList.toggle("active");
     burger.classList.toggle("active");
 })
+
+const navHome = document.querySelector('.element-home');
+const navAbout = document.querySelector('.element-about');
+const navWork = document.querySelector('.element-work');
+
+
+navHome.addEventListener("click", function(){
+    if(mobileNav.classList.contains("active")){
+        mobileNav.classList.toggle("active");
+        burger.classList.toggle("active");
+    }
+    else{
+    }
+})
+navAbout.addEventListener("click", function(){
+    if(mobileNav.classList.contains("active")){
+        mobileNav.classList.toggle("active");
+        burger.classList.toggle("active");
+    }
+    else{
+    }
+})
+navWork.addEventListener("click", function(){
+    if(mobileNav.classList.contains("active")){
+        mobileNav.classList.toggle("active");
+        burger.classList.toggle("active");
+    }
+    else{
+    }
+})
